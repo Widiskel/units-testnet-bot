594fcba0a9d195d54fc36f2f50b38d8715639b25
Widianto Eka saputro <97203329+Widiskel@users.noreply.github.com>
2024-10-22 22:36:56 +0700

    Update accounts_tmp.js

--- accounts/accounts_tmp.js
+++ accounts/accounts_tmp.js
@@ -2,9 +2,14 @@
  * Private key list file
  * write your private key here like this
  * export const privateKey = [
- *   "PK1",
- *   "PK2",
- *   "PK3",
+ *   {
+ *     pk: "YOUR PRIVATE KEY HERE",
+ *     query: "query_id=xxxxx or user_id=xxxx from your telegram",
+ *   },
+ *   {
+ *     pk: "YOUR PRIVATE KEY HERE",
+ *     query: "query_id=xxxxx or user_id=xxxx from your telegram",
+ *   }
  * ];
  */
 
